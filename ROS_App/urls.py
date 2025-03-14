f629af70d0da9f5a9afa50357276f816b2ff8eb2
“Kazim <2917569a@student.gla.ac.uk>
2025-03-14 12:47:29 +0000

    register.html

--- ROS_App/urls.py
+++ ROS_App/urls.py
@@ -3,11 +3,7 @@ from . import views
 from .views import login_view
 
 urlpatterns = [
-<<<<<<< HEAD
     path('login/', views.login_view, name='login'), 
-=======
-    path('', views.login_view, name='login'), 
->>>>>>> 09cd31530b41322c48d3c0fc6d95748b0b98dc46
     path('home/', views.home_view, name='home'), 
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),

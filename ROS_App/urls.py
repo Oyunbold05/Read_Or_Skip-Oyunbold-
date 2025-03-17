d6e3c476006136975f9ae9da2f409e86a7494cd1
Oyunbold05 <148216243+Oyunbold05@users.noreply.github.com>
2025-03-17 21:32:17 +0000

    Merge Request (#1)
    * wireframe 2
    
    * CSS for about us almost done + htmls for contact us, about us and FAQ
    
    * Fixed the URLs, pages communictae among each other
    
    * CSS for About Us done
    
    * Resolved merge conflicts and updated views and models for Wireframe 2
    
    * Wireframe 2 backend successful
    
    ---------
    
    Co-authored-by: Azra Miray Atik <2890296A@student.gla.ac.uk>
    Co-authored-by: Karla-Mihai <2880904M@student.gla.ac.uk>

--- ROS_App/urls.py
+++ ROS_App/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from . import views
-from .views import login_view
 
 urlpatterns = [
     path('login/', views.login_view, name='login'), 
     path('home/', views.home_view, name='home'), 
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
+    path('aboutUs/',views.aboutUs_view, name='aboutUs'),
+    path('faq/', views.faq_view, name='faq'),
+    path('contactUs/', views.contactUs_view, name='contactUs'),
+    path('myAccount/',views.myAccount_view, name="myAccount"),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('book/<int:book_id>/add_to_tbr/', views.add_to_tbr, name='add_to_tbr'),
+    path('tbr/', views.tbr_list, name='tbr_list'),
+    path('book/<int:review_id>/delete_review/', views.delete_review, name='delete_review'),
+    path('book/<int:review_id>/edit_review/', views.edit_review, name='edit_review'),
+    path('book/<int:book_id>/delete_from_tbr/', views.delete_from_tbr, name='delete_from_tbr'),
 ]
